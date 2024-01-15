bec129312dd737b2e469fab4f22dde92bcb73221
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2024-01-15 15:36:21 +0100

    Implement environments mechanism

---
+++ src/BOMA.RTR.Client/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { APP_VERSION } from '../app/version';
+
+export const environment = {
+  appVersion: `${APP_VERSION}`,
+  apiEndpoint: 'http://192.168.0.161:83',
+};
---
+++ src/BOMA.RTR.Client/src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { APP_VERSION } from '../app/version';
+
+export const environment = {
+  appVersion: `${APP_VERSION}-dev`,
+  apiEndpoint: 'http://localhost:5133',
+};
