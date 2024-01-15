f8f93ed94633f35aaf0756ff6d7c6273cb57b085
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2024-01-15 09:24:45 +0100

    Remove template enpoint

--- src/BOMA.RTR.Api/Program.cs
+++ src/BOMA.RTR.Api/Program.cs
@@ -30,26 +30,6 @@ app.UseHttpsRedirection();
 
 app.UseHealthChecks("/healthz");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
 app.MapGet("/entryExitTimes", (IRogerFileService rogerFileService) =>
     {
         var records = rogerFileService.ParseRecords();
@@ -60,8 +40,3 @@ app.MapGet("/entryExitTimes", (IRogerFileService rogerFileService) =>
 .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
